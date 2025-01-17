54b41747441c32ea52ed3a752547f3d6ec90880a
Liam Skirrow <skirrowliam@gmail.com>
2025-01-17 18:40:02 +0000

    Initial commit, include all QTCreator boilerplate

---
+++ build/Desktop-Debug/CMakeFiles/LangTrainer.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LangTrainer_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/LangTrainer_autogen.dir/ParseCache.txt"
+  "LangTrainer_autogen"
+  "CMakeFiles/LangTrainer.dir/LangTrainer_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/LangTrainer.dir/LangTrainer_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/LangTrainer.dir/main.cpp.o"
+  "CMakeFiles/LangTrainer.dir/main.cpp.o.d"
+  "CMakeFiles/LangTrainer.dir/mainwindow.cpp.o"
+  "CMakeFiles/LangTrainer.dir/mainwindow.cpp.o.d"
+  "LangTrainer"
+  "LangTrainer.pdb"
+  "LangTrainer_autogen/mocs_compilation.cpp"
+  "LangTrainer_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LangTrainer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/Desktop-Debug/CMakeFiles/LangTrainer_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LangTrainer_autogen"
+  "LangTrainer_autogen/include/ui_mainwindow.h"
+  "LangTrainer_autogen/mocs_compilation.cpp"
+  "LangTrainer_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/LangTrainer_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
