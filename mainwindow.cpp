b7adb3d22519257d3daf9ef7d242228f94492453
Liam Skirrow <skirrowliam@gmail.com>
2025-01-17 20:00:43 +0000

    Rm cached the old build files, added button in GUI to print out a debug message on click

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -12,3 +14,9 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_clicked()
+{
+    qDebug() << "howdy ho";
+}
+
